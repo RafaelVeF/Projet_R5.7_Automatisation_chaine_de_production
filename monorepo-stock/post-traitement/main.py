4050019f1f3f9445d9ee69d697b809dacc997cca
lMasshu <baptistedevsh@gmail.com>
2026-09-24 15:22:27 +0200

    feat(post-traitement): ajout de l’API de décodage QR avec upload d’image

--- monorepo-stock/post-traitement/main.py
+++ monorepo-stock/post-traitement/main.py
@@ -1,1 +1,31 @@
 # fichier main.py du module post-traitement
+from fastapi import FastAPI, File, UploadFile
+import cv2 as cv
+import numpy as np
+
+app = FastAPI()
+
+
+@app.post("/api/decode")
+async def decode_image(file: UploadFile = File(...)):
+    contenue = await file.read()
+
+    # Convertir le contenu en tableau numpy
+    nptab = np.frombuffer(contenue, np.uint8)
+
+    # Lire l'image à partir du tableau numpy
+    image = cv.imdecode(nptab, cv.IMREAD_COLOR)
+
+    detecteur = cv.QRCodeDetector()
+    data, points, _ = detecteur.detectAndDecode(image)
+
+    if data:
+        return {
+            "status" : "ok",
+            "data": data
+        }
+
+    return {
+        "status" : "error",
+        "data": None
+    }
